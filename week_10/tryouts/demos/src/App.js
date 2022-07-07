d0a9dd346f5ecaa2e1f5422299b1556dfc0aaed6
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-07-07 19:47:33 +0100

    adding tryouts to week_10

---
+++ week_10/tryouts/demos/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Dropdown } from './Dropdown.js'
+import { Element } from './Element.js'
+
+function App() {
+  return (
+    <>
+    <Dropdown />
+    <Element />
+    </>
+  );
+}
+
+export default App;
